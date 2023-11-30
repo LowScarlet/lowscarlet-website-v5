3393247b58525151f279836dcd2dda3ab06027b2
Low_Scarlet <64282946+LowScarlet@users.noreply.github.com>
2023-11-30 12:51:42 +0700

    Delete Backend

--- backend/src/utils/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-function NotFoundException() {
-  this.status = 400;
-  this.message = 'utils.middlewares.not-found';
-}
-
-module.exports = (req, res, next) => {
-  throw new NotFoundException();
-};
--- backend/src/utils/sendRefreshToken.js
+++
@@ -1,9 +0,0 @@
-function sendRefreshToken(res, token) {
-  res.cookie('refreshToken', token, {
-    httpOnly: true,
-    sameSite: true,
-    path: '/api/v1/auth',
-  });
-}
-
-module.exports = { sendRefreshToken };
